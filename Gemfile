87acd29fd3f8773795f44243746fe15b28fb87cb
Vo Duc Loi <loi.vd@neo-lab.vn>
2021-01-25 17:29:14 +0700

    add assets for webpack

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem "roo", "~> 2.8.0"
 gem 'caxlsx'
 gem 'caxlsx_rails'
+gem 'wicked_pdf'
+gem 'wkhtmltopdf-binary'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -14,11 +14,20 @@ class ReportsController < ApplicationController
     data_original = @report.original_sheet.parse
     @carton = data_original.select {|row| row.include?(params[:dh])}[0]
     @items = @report.get_items_by(params[:dh])
+
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "carton", encoding: 'UTF-8'
+      end
+    end
   end
 
   def create
     @new_report = Report.new(report_params)
-    @new_report.save
+    if @new_report.save
+      redirect_to reports_path
+    end
   end
 
   def download
