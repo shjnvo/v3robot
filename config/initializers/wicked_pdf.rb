4b83781ef6dbba21a85ae85f2a79455d7e11ed48
Vo Duc Loi <loi.vd@neo-lab.vn>
2021-01-26 11:39:11 +0700

    add assets for webpack

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -13,9 +13,9 @@ WickedPdf.config = {
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+  exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
-  exe_path: "#{Rails.root}/bin/wkhtmltopdf"
+  # exe_path: "#{Rails.root}/bin/wkhtmltopdf"
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
